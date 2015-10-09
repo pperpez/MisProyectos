3176447b7209baad5bf7c12b4d63b5d816c8f5b4
Altair Clase <carrascoruedaadrian@gmail.com>
2015-10-09 12:15:51 +0200

    i

--- Hola/src/hola/Hola.java
+++ Hola/src/hola/Hola.java
@@ -5,7 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
-	System.out.println("Adiós");
+	System.out.println("Adiiós");
 	System.out.println("HUE");
 	System.out.println(" De locos");
 	}
